d5eecdc45a3e45bb7f61a419e50fff78cbae53f2
Raju9934 <raju.gupta10u@gmail.com>
2025-01-14 15:56:05 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,6 +25,7 @@ function Login() {
       <h2>Login</h2>
       <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
       <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+  <h2> This is new heading</h2>
       <button type="submit">Login</button>
     </form>
   );
